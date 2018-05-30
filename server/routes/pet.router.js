a42c06b8b6e00750de0cc95958e074df5ada67e3
Dane Smith <doctorhowser@gmail.com>
2018-05-30 11:48:30 -0500

    reset

--- server/routes/pet.router.js
+++ server/routes/pet.router.js
@@ -2,42 +2,28 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-// This route should return the logged in users pets
+// This route *should* return the logged in users pets
 router.get('/', (req, res) => {
     console.log('/pet GET route');
     console.log('is authenticated?', req.isAuthenticated());
     console.log('user', req.user);
-    if(req.isAuthenticated()) {
-        let queryText = `SELECT * FROM "pet" WHERE "person_id" = $1;`;
-        pool.query(queryText, [req.user.id]).then((result) => {
-            res.send(result.rows);
-        }).catch((error) => {
-            console.log(error);
-            res.sendStatus(500);
-        });
-    } else {
-        res.sendStatus(403); // Unauthorized!
-    }
-
+    let queryText = `SELECT * FROM "pet"`;
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    });
 });
 
-// This route should add a pet for the logged in user
+// This route *should* add a pet for the logged in user
 router.post('/', (req, res) => {
     console.log('/pet POST route');
     console.log(req.body);
     console.log('is authenticated?', req.isAuthenticated());
     console.log('user', req.user);
-    if(req.isAuthenticated()) {
-        let queryText = `INSERT INTO "pet" ("firstname", "person_id") VALUES
-                         ($1, $2)`;
-        pool.query(queryText, [req.body.firstname, req.user.id]).then((result) => {
-            res.sendStatus(201);
-        }).catch((err) => {
-            res.sendStatus(500);
-        })
-    } else {
-        res.sendStatus(403);
-    }
+    res.sendStatus(200);
+    
 });
 
 module.exports = router;
